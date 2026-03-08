0a6260ca580a98a47166f3d377c23319f592373d
unknown <brianzioki100@gmail.com>
2026-03-08 20:16:09 +0300

    Secure Paystack key in .env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const config = {
-    PAYSTACK_PUBLIC_KEY: 'pk_live_74ae5ea945755ebdcccb7c4bdee6de2f2381464b',
+    PAYSTACK_PUBLIC_KEY: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY || 'pk_live_74ae5ea945755ebdcccb7c4bdee6de2f2381464b',
 };
